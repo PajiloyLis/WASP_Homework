69c0dbeb7661a413fd4906d8de5a41efdcc233d1
PajiloyLis <118011470+PajiloyLis@users.noreply.github.com>
2022-11-18 17:14:01 +0300

    Add files via upload

---
+++ Волшебные числа.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            for(int i=9; i>=1; --i)
+            {
+                for(int j=i-1; j>=0; --j)
+                {
+                    for(int k=j-1; k>=0; --k)
+                    {
+                        for(int q=k-1; q>=0; --q)
+                        {
+                            string ans = $"{i}{j}{k}{q}";
+                            Console.WriteLine(ans);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Распаковка шортов.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = Convert.ToInt64(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("Incorrect input.");
+            }
+            else
+            {
+                long mask = (1 << 16) - 1;
+                Console.Write((number & mask) + " ");
+                Console.Write(((number & (mask << 16))>>16) + " ");
+                Console.Write(((number & (mask << 32))>>32) + " ");
+                Console.Write(((number & (mask << 48))>>48) + " ");
+            }
+        }
+    }
+}
