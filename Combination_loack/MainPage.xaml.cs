a8b50e6c1cc9384d21b4018f7fd08b0d00b76a46
Bugakov Ivan <118011470+PajiloyLis@users.noreply.github.com>
2022-12-03 21:11:19 +0300

    Add files via upload

---
+++ Combination_loack/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace Combination_lock;
+
+public partial class MainPage : ContentPage
+{
+	string _code = "0601";
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void DigitClicked(object sender, EventArgs e)
+	{
+		if (DisplayLabel.Text != "Верно!")
+		{
+			DisplayLabel.Text += (sender as Button).Text;
+		}
+	}
+	private void ClearClicked(object sender, EventArgs e)
+	{
+		if (DisplayLabel.Text != "Верно!")
+		{
+			DisplayLabel.Text = "";
+		}
+	}
+	private void ConfirmClicked(object sender, EventArgs e)
+	{
+		if (DisplayLabel.Text != "Верно!")
+		{
+			if (DisplayLabel.Text.Split('\n').Last() == "")
+			{
+				return;
+			}
+			if (DisplayLabel.Text.Split('\n').Last() == _code)
+			{
+				DisplayLabel.Text = "Верно!";
+
+			}
+			else
+			{
+				DisplayLabel.Text = "";
+			}
+		}
+	}
+}
+
